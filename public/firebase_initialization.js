ac15e691cb8bd16338746c6a29d9956ebefa99af
Manav Sarkar <manavsarkar07@gmail.com>
2021-04-10 19:29:00 +0530

    manav image blog separate initialization

---
+++ public/firebase_initialization.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyB2vP5Ali-GsfYBV5m2P7dmK84-1IaaLvw",
+	authDomain: "image-sharing-app-tutorial.firebaseapp.com",
+	databaseURL: "https://image-sharing-app-tutorial-default-rtdb.firebaseio.com/",
+	projectId: "image-sharing-app-tutorial",
+	storageBucket: "image-sharing-app-tutorial.appspot.com",
+	messagingSenderId: "535489543231",
+	appId: "1:535489543231:web:d083ea04d3eca18c420c8a",
+	measurementId: "G-67NFYJV81E"
+};
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
